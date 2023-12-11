3de897adb5d447faf4496cb54f742b6d1aea90cc
JeongBeomi <qja367@gmail.com>
2023-12-11 22:02:20 +0900

    feat : add BorrowingService

--- LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/service/BorrowingService.java
+++ LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/service/BorrowingService.java
@@ -1,4 +1,10 @@
 package com.example.librarymanagement.borrowing.service;
 
+import com.example.librarymanagement.books.dto.UpdateBooksRequest;
+import com.example.librarymanagement.books.dto.UpdateBooksResponse;
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingRequest;
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingResponse;
+
 public interface BorrowingService {
+    CreateBorrowingResponse createBorrowing(CreateBorrowingRequest request);
 }
--- LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/service/BorrowingServiceImpl.java
+++ LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/service/BorrowingServiceImpl.java
@@ -1,6 +1,13 @@
 package com.example.librarymanagement.borrowing.service;
 
+import com.example.librarymanagement.books.entity.Books;
 import com.example.librarymanagement.books.repository.BooksRepository;
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingRequest;
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingResponse;
+import com.example.librarymanagement.borrowing.entity.Borrowing;
+import com.example.librarymanagement.exception.exceptino_message.NotFoundExceptionMessage;
+import com.example.librarymanagement.user.entity.User;
+import com.example.librarymanagement.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +17,28 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BorrowingServiceImpl implements BorrowingService {
     private final BooksRepository booksRepository;
+    private final UserRepository userRepository;
 
 
+    @Override
+    @Transactional
+    public CreateBorrowingResponse createBorrowing(CreateBorrowingRequest request) {
+        User findUser = userRepository.findByEmail(request.getEmail()).orElseThrow(
+                () -> new NotFoundExceptionMessage(NotFoundExceptionMessage.NOT_FOUND_USER)
+        );
+
+        Books findBooks = booksRepository.findById(request.getBooksId()).orElseThrow(
+                () -> new NotFoundExceptionMessage((NotFoundExceptionMessage.NOT_FOUND_BOOKS))
+        );
+
+        // 이미 대여되어 대여가 불가능한 상태
+        if (findBooks.isBorrowing()) {
+            throw new NotFoundExceptionMessage(NotFoundExceptionMessage.EXIST_BORROWING);
+        }
+
+        Borrowing borrowing = Borrowing.create(findUser, findBooks);
+        Books books = findBooks.update();
+
+        return CreateBorrowingResponse.create(books, findUser, borrowing);
+    }
 }
