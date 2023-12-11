c32e81ddf9c9176a0f3794a30e74920c1e7dbb55
JeongBeomi <qja367@gmail.com>
2023-12-11 18:55:39 +0900

    feat : add exception

---
+++ LibraryManagement/src/main/java/com/example/librarymanagement/books/dto/UpdateBooksRequest.java
@@ -0,0 +1,4 @@
+package com.example.librarymanagement.books.dto;
+
+public class UpdateBooksRequest {
+}
---
+++ LibraryManagement/src/main/java/com/example/librarymanagement/exception/ExceptionResponse.java
@@ -0,0 +1,35 @@
+package com.example.librarymanagement.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.project.fri.exception fileName       : ExceptionResponse date           :
+ * 2023-04-19 description    :
+ */
+@Getter
+@Builder
+public class ExceptionResponse {
+  private final LocalDateTime timestamp;
+  //  private final String path;
+  private final String path;
+  private final HttpStatus httpStatus;
+  private final String message;
+
+  public static ExceptionResponse createExceptionResponse(Exception e, HttpStatus httpStatus, ServerHttpRequest request){
+//    LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+    final String message = e.getMessage();
+    final String path = request.getPath().pathWithinApplication().value();
+    return ExceptionResponse.builder()
+        .timestamp(LocalDateTime.now())
+        .path(path)
+        .httpStatus(httpStatus)
+        .message(message)
+        .build();
+  }
+}
+
