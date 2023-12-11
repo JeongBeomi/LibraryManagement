71b42dfb4219495cdd887f7ed9f86ba6b6193d60
JeongBeomi <qja367@gmail.com>
2023-12-11 22:01:49 +0900

    feat : add NotFoundExceptionMessage

--- LibraryManagement/src/main/java/com/example/librarymanagement/exception/exceptino_message/NotFoundExceptionMessage.java
+++ LibraryManagement/src/main/java/com/example/librarymanagement/exception/exceptino_message/NotFoundExceptionMessage.java
@@ -7,7 +7,9 @@ package com.example.librarymanagement.exception.exceptino_message;
 public class NotFoundExceptionMessage extends RuntimeException{
 
   public static final String EXIST_USER = "이미 존재하는 유저입니다.";
+  public static final String EXIST_BORROWING = "이미 대여중인 책입니다.";
   public static final String NOT_FOUND_BOOKS = "존재하지 않는 책입니다.";
+  public static final String NOT_FOUND_USER = "존재하지 않는 유저입니다.";
 
   public static final String NOT_FOUND_ROOM_LIST = "존재하지 않는 방 리스트 입니다.";
 
