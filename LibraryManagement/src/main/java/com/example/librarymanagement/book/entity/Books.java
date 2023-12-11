5f0c6e84a3cccf256a86f93e19ccbf93c6208f62
JeongBeomi <qja367@gmail.com>
2023-12-11 15:03:04 +0900

    feat : add entity books

---
+++ LibraryManagement/src/main/java/com/example/librarymanagement/book/entity/Books.java
@@ -0,0 +1,47 @@
+package com.example.librarymanagement.book.entity;
+
+import com.example.librarymanagement.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="book")
+public class Books extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private String isbn;
+
+    @NotNull
+    private boolean isBorrowing;
+
+    //==비즈니스 로직==//
+
+    //생성 메서드
+    public static Books create(String title, String author, String isbn) {
+        return Books.builder()
+                .title(title)
+                .author(author)
+                .isbn(isbn)
+                .isBorrowing(true)
+                .build();
+
+    }
+
+}
