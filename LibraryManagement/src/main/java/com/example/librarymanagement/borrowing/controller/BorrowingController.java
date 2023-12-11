9598513186124e14e2ec7d02f3ae105837b5a43c
JeongBeomi <qja367@gmail.com>
2023-12-11 22:02:30 +0900

    feat : add BorrowingController

--- LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/controller/BorrowingController.java
+++ LibraryManagement/src/main/java/com/example/librarymanagement/borrowing/controller/BorrowingController.java
@@ -1,11 +1,23 @@
 package com.example.librarymanagement.borrowing.controller;
 
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingRequest;
+import com.example.librarymanagement.borrowing.dto.CreateBorrowingResponse;
+import com.example.librarymanagement.borrowing.service.BorrowingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/borrowing")
 public class BorrowingController {
+    private final BorrowingService borrowingService;
+
+    @PostMapping("/{bookId}")
+    public ResponseEntity<CreateBorrowingResponse> createBooks(@RequestBody CreateBorrowingRequest request) {
+        CreateBorrowingResponse createBorrowingResponse = borrowingService.createBorrowing(request);
+        return ResponseEntity.created(URI.create("/books")).body(createBorrowingResponse);
+    }
 }
